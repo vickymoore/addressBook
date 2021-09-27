1aef20aacbc9c9cc5f06e425c9a8c6eacbe3e177
praise <allisonsandra276@gmail.com>
2021-09-27 10:23:00 +0100

    last-commit

--- js/scripts.js
+++ js/scripts.js
@@ -25,22 +25,34 @@ AddressBook.prototype.deleteContact = function(id) {
     return false;
   }
   delete this.contacts[id];
-  return true;
+  confirm("Are You Sure");
 };
 
 // Business Logic for Contacts ---------
-function Contact(firstName, lastName, phoneNumber, email, phoneAdd, homeAdd) {
+function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
-  this.email= email;
-  this.phoneAdd= phoneAdd;
-  this.homeAdd= homeAdd;
+  
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
+
+Contact.prototype.addNumber = function (phoneNumber) {
+  this.phoneNumber = phoneNumber;
+  return true;
+}
+
+Contact.prototype.addEmail = function (emailAddress) {
+  this.email = emailAddress;
+  return true;
+}
+
+Contact.prototype.addAddress = function (physicalAddress) {
+  this.address = physicalAddress;
+}
 
 
 //Business Logic For PhoneNumber, EmailAddress & PhysicalAddress:-----
@@ -82,10 +94,18 @@ function showContact(contactId) {
   $("#show-contact").show();
   $(".first-name").html(contact.firstName);
   $(".last-name").html(contact.lastName);
-  $(".phone-number").html(contact.phoneNumber );
-  $(".email").html(contact.email);
-  $(".phone-add").html(contact.phoneAdd);
-  $(".home-add").html(contact.homeAdd);
+  $(".phonemobile").html(contact.phoneNumber.mobile);
+  $(".phonehome").html(contact.phoneNumber.home);
+  $(".phonework").html(contact.phoneNumber.work);
+  $(".phoneothers").html(contact.phoneNumber.other);
+  $(".emailmobile").html(contact.email.mobile);
+  $(".emailhome").html(contact.email.home);
+  $(".emailwork").html(contact.email.work);
+  $(".emailothers").html(contact.email.other);
+  $(".homeaddress").html(contact.address.home);
+  $(".workaddress").html(contact.address.work);
+  $(".otheraddress").html(contact.address.other);
+
   let buttons = $("#buttons");
   buttons.empty();
   buttons.append("<i   class='fas fa-trash delvic' id=" +  + contact.id + "></i>");
@@ -109,20 +129,69 @@ $(document).ready(function() {
     event.preventDefault();
     const inputtedFirstName = $("input#new-first-name").val();
     const inputtedLastName = $("input#new-last-name").val();
-    const inputtedPhoneNumber = $("input#new-phone-number").val();
-    const inputtedEmail = $("input#new-email").val();
-    const inputtedPhoneAddress = $("input#new-phone-address").val();
-    const inputtedHomeAddresss = $("input#new-home-address").val();
+    let newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    const inputtedMobileNumber = $("input#new-mobile-number").val();
+    const inputtedHomeNumber = $("input#new-home-number").val();
+    const inputtedWorkNumber = $("input#new-work-number").val();
+    const inputtedOtherNumber = $("input#new-other-number").val();
+
+    let newPhoneNumber = new PhoneNumber(inputtedMobileNumber, inputtedHomeNumber, inputtedWorkNumber, inputtedOtherNumber);
+
+    const inputtedMobileEmail = $("input#new-mobile-email").val();
+    const inputtedHomeEmail = $("input#new-home-email").val();
+    const inputtedWorkEmail = $("input#new-work-email").val();
+    const inputtedOtherEmail = $("input#new-other-email").val();
+
+    let newEmail = new EmailAddress(inputtedMobileEmail, inputtedHomeEmail, inputtedWorkEmail, inputtedOtherEmail);
+
+    const inputtedHomeAddress = $("input#new-home-address").val();
+    const inputtedWorkAddress = $("input#new-work-address").val();
+    const inputtedOtherAddress = $("input#new-other-address").val();
+
+    let newPhysicalAddress = new PhysicalAddress(inputtedHomeAddress, inputtedWorkAddress, inputtedOtherAddress);
+
 
     $("input#new-first-name").val("");
-  $("input#new-last-name").val("");
-  $("input#new-phone-number").val("");
-  $("input#new-email").val("");
-  $("input#new-phone-address").val("");
-  $("input#new-home-address").val("");
+    $("input#new-middle-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-mobile-number").val("");
+    $("input#new-home-number").val("");
+    $("input#new-work-number").val("");
+    $("input#new-other-number").val("");
+    $("input#new-mobile-email").val("");
+    $("input#new-home-email").val("");
+    $("input#new-work-email").val("");
+    $("input#new-other-email").val("");
+    $("input#new-home-address").val("");
+    $("input#new-work-address").val("");
+    $("input#new-other-address").val("");
 
-    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber,  inputtedEmail, inputtedPhoneAddress, inputtedHomeAddresss);
+    newContact.addNumber(newPhoneNumber);
+    newContact.addEmail(newEmail);
+    newContact.addAddress(newPhysicalAddress);
     addressBook.addContact(newContact);
     displayContactDetails(addressBook);
+
+    $(".hidden1").hide();
+    $(".hidden2").hide();
+    $(".hidden3").hide();
   });
-});
+
+  $("#same1").click(function () {
+    $(".hidden1").toggle();
+  })
+
+  $("#same2").click(function () {
+    $(".hidden2").toggle();
+  })
+
+  $("#same3").click(function () {
+    $(".hidden3").toggle();
+  })
+
+  $("#done").click(function(){
+    $("#show-contact").hide();
+  })
+
+  });
